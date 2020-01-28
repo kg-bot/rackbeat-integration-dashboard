5daa74ac8347866542cc4c10b59892a8dfea4101
Stefan Ninic <ninicstefan94@gmail.com>
2020-01-28 14:54:00 +0100

    Move cleanup code to artisan command directly

--- src/Console/Commands/ClearOldJobs.php
+++ src/Console/Commands/ClearOldJobs.php
@@ -2,8 +2,8 @@
 
 namespace KgBot\RackbeatDashboard\Console\Commands;
 
+use Carbon\Carbon;
 use Illuminate\Console\Command;
-use KgBot\RackbeatDashboard\Classes\DashboardJobExport;
 use KgBot\RackbeatDashboard\Models\Job;
 
 class ClearOldJobs extends Command
@@ -40,12 +40,42 @@ class ClearOldJobs extends Command
     public function handle()
     {
 
-        Job::create([
+        \Log::debug('Running clear job');
+        $handle = fopen(storage_path('app/jobs-' . Carbon::now()->toDateString() . '.csv'), 'w');
+        $count = 0;
 
-            'command' => DashboardJobExport::class,
-            'args' => [],
-            'queue' => config('queue.connections.redis.queue'),
-            'title' => 'Export old jobs',
+        fputcsv($handle, [
+            'id',
+            'command',
+            'created_by',
+            'created_at',
+            'finished_at',
+            'title',
+            'rackbeat_account',
         ]);
+
+        Job::whereState('success')->whereDate('created_at', '<', Carbon::now()->subDays(7))->chunk(5000, function ($jobs) use ($handle, $count) {
+
+            \Log::debug('Chunk fetched.');
+            foreach ($jobs as $job) {
+                fputcsv($handle, [
+
+                    $job->id,
+                    $job->command,
+                    $job->created_by,
+                    $job->created_at,
+                    $job->finished_at,
+                    $job->title,
+                    ($job->owner !== null) ? ($job->owner->rackbeat_user_account_id ?? '') : '',
+                ]);
+            }
+
+            $job->delete();
+            $count++;
+        });
+
+        fclose($handle);
+
+        \Log::debug($count . 'jobs exported and deleted');
     }
 }
