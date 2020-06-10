bf3e02ee7d2966d11c246201cb126affa091a7d5
Stefan Ninic <ninicstefan94@gmail.com>
2020-06-10 16:40:14 +0200

    Fixed message to include error

--- src/Observers/JobObserver.php
+++ src/Observers/JobObserver.php
@@ -34,7 +34,7 @@ class JobObserver
 
             dispatch(new $job->command($job, ...$job->args))->onQueue($job->queue)->onConnection(\Config::get('queue.default', 'redis'))->delay($delay);
 		} catch ( \Throwable $e ) {
-			\Log::error( 'Can\'t dispatch job ' . $job->command ?? $job->id . ', error: ' . $e->getMessage() );
+			\Log::error( 'Can\'t dispatch job ' . ( $job->command ?? $job->id ) . ', error: ' . $e->getMessage() );
 			$job->delete();
 		}
 	}
